884e58572b655bb1b44d9d50590e8b242d23d4d3
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-03-23 13:26:22 +0700

    register done

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async function main() {
 
 
 
-    var indexRouter = require('./routes/index');
+    var indexRouter = require('./routes/index')(db);
     var todosRouter = require('./routes/todos')(db);
 
     var app = express();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+const secretKey = "rubicamp"
+const saltRounds = 10
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (db) {
 
-module.exports = router;
+  router.post("./register", async function (req, res, next) {
+    try {
+      const { email, fullname, password } = req.body
+
+      const hash = await bcrypt.hashSync(password, saltRounds);
+      const userCreated = await collection.insertOne({ email, fullname, password: hash })
+      const user = await collection.findOne(userCreated.insertedId)
+      var token = jwt.sign(user, secretKey);
+      res.json(token)
+    } catch (err) {
+      res.json(err)
+    }
+  });
+
+  router.post("./auth", function (req, res, next) {
+    const { email, password } = req.body
+  });
+
+  return router;
+}
