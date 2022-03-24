df2e9f70065c4431222161132d25de70b8a8c53f
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-03-24 10:51:56 +0700

    connecting to app react

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { MongoClient } = require('mongodb');
+const cors = require("cors")
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
@@ -32,6 +33,7 @@ async function main() {
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(cors())
 
     app.use('/', indexRouter);
     app.use('/todos', todosRouter);
